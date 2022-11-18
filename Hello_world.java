1d66e359899244e48a732b0493f572f7a700c377
Sandeep Kumar Jaiswal <jaiswalsandeep508@gmail.com>
2022-11-18 12:06:36 +0530

    First Commit

---
+++ Hello_world.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Hello_world {
+  public static void main(String args[]){
+    // System.out.print("Hello World!");
+  Scanner sc=new Scanner(System.in);
+  float r = sc.nextFloat();
+  
+  double a = 3.14*r*r;
+  System.out.print(a);
+  }
+}
