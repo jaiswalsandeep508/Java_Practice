628af5e3a9d835a21c61dda6cbb0836fd858388d
Sandeep Kumar Jaiswal <jaiswalsandeep508@gmail.com>
2022-11-18 12:24:03 +0530

    Area of Square

---
+++ AreaSquare.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class AreaSquare {
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter Side Of Square ❤");
+    int a = sc.nextInt();
+    int area = a*a;
+    System.out.println(area);
+  }
+}
