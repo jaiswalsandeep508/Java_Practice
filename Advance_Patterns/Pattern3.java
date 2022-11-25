db4a9b533bbc5fc1483704bcdcced1d6d2171c6a
Sandeep Kumar Jaiswal <jaiswalsandeep508@gmail.com>
2022-11-25 13:28:21 +0530

    Print Numbers (Floyd's Triangle)

--- Advance_Patterns/Pattern3.java
+++ Advance_Patterns/Pattern3.java
@@ -1,22 +1,25 @@
 /*----------------\
-1 2 3 4 5
-1 2 3 4
-1 2 3
-1 2
 1
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
+
 ----------------*/ 
 
 import java.util.*;
 
-public class Pattern2{
+public class Pattern3{
   public static void main(String args[]){
     Scanner sc = new Scanner(System.in);
     System.out.print("Enter any number: ");
     int n = sc.nextInt();
+    int count = 1;
 
     for(int i=1;i<=n;i++){
-      for(int j=1;j<=n-i+1;j++){
-        System.out.print(j + " ");
+      for(int j=1;j<=i;j++){
+        System.out.print(count + " ");
+        count++;
       }
       System.out.println();
     }
