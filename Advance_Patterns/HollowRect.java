7a46655ce11109d8a952fa95c7a254659fa636aa
Sandeep Kumar Jaiswal <jaiswalsandeep508@gmail.com>
2022-11-25 11:50:11 +0530

    Print Star (Hollow Rectangle)

---
+++ Advance_Patterns/HollowRect.java
@@ -0,0 +1,32 @@
+/*---------------------
+**********
+*        *
+*        *
+*        *
+**********
+----------------------*/ 
+
+import java.util.*;
+
+public class HollowRect{
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter any Number: ");
+    int n = sc.nextInt();
+
+    for(int i=1;i<=n;i++){
+      for(int j=1;j<=n+1;j++){
+        if(i==1 || i==n){
+          System.out.print("* ");
+        }else{
+          if(j==1 || j==n+1)
+          System.out.print("* ");
+          else{
+            System.out.print("  ");
+          }
+        }
+      }
+      System.out.println();
+    }
+  }
+}
